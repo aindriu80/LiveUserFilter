b7b13cfc44cecdc649a49ba0aa371d9c0068f6d9
aindriu80 <aindriu80@gmail.com>
2021-05-10 15:32:27 +0100

    Filtering the users displayed based on search criteria

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,9 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
+
 getData()
+filter.addEventListener('input', (e) => filterData(e.target.value))
 async function getData() {
   const res = await fetch('https://randomuser.me/api?results=50')
 
@@ -22,5 +24,15 @@ async function getData() {
       </div>
       `
     result.appendChild(li)
+  })
+}
+
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide')
+    } else {
+      item.classList.add('hide')
+    }
   })
 }
